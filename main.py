8ab6084ad584b83482661812f35bc4792c6fdb5c
Esau Salvador Reyes Tellez <sal.reyes05@gmail.com>
2025-09-14 21:58:05 -0600

    imports

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import data
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.support.wait import WebDriverWait
 import data
 
